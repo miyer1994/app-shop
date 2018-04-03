1616ed1a0410f252dc4bca9cc87ee0658100d9b4
unknown <miyer angel@LAPTOP-4HEFO04Q.com>
2018-04-02 21:08:04 -0500

    Base project

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="header header-filter" style="background-image: url('{{ asset('assets/img/city.jpg')}}'); background-size: 
+cover; background-position: top center;">
+</div>
+@endsection
